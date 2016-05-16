ae766423d3c5935b1140ccba30838609c496a70f
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-16 20:37:30 +0500

    Add Project model

--- face/models.py
+++ face/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    picture = models.ImageField()
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
