5957a07a2a6fb419765036bfcf9fd8d7c2508a25
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-16 20:47:25 +0500

    Link Project model to admin panel

--- face/admin.py
+++ face/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,
+            {'fields':
+                ['name', 'picture', 'description']
+            }
+        )
+    ]
+    list_display = ('name',)
