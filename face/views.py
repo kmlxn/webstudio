b7f66f572727f9c8c20d1949a5b27d804afaa0ee
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-18 17:25:59 +0500

    Render team members from db

--- face/views.py
+++ face/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Project
+from .models import Project, TeamMember
 
 def get_index_page(request):
     return render(request, 'face/index.html', {
         'projects': Project.objects.all(),
+        'team_members': TeamMember.objects.all(),
     })
