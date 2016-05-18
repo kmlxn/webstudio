f053d5be310f45f617ad1bf63c6ea28a70e8c54a
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-18 18:59:14 +0500

    Implement customer message send

--- face/static/face/js/contact_me.js
+++ face/static/face/js/contact_me.js
@@ -11,20 +11,23 @@ $(function() {
             var name = $("input#name").val();
             var email = $("input#email").val();
             var phone = $("input#phone").val();
+            var csrfmiddlewaretoken = $("input[name='csrfmiddlewaretoken']").val();
             var message = $("textarea#message").val();
+            var formAction = $form.attr('action');
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: formAction,
                 type: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
-                    message: message
+                    message: message,
+                    csrfmiddlewaretoken: csrfmiddlewaretoken
                 },
                 cache: false,
                 success: function() {
--- face/views.py
+++ face/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from .models import Project, TeamMember
+from django.http import HttpResponse
+from .models import Project, TeamMember, CustomerMessage
+
 
 def get_index_page(request):
-    return render(request, 'face/index.html', {
-        'projects': Project.objects.all(),
-        'team_members': TeamMember.objects.all(),
-    })
+    if request.method == 'POST':
+        CustomerMessage.objects.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            phone=request.POST['phone'],
+            message=request.POST['message'],
+        )
+        return HttpResponse('ok')
+    else:
+        return render(request, 'face/index.html', {
+            'projects': Project.objects.all(),
+            'team_members': TeamMember.objects.all(),
+        })
