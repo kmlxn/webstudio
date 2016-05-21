e6cc0289dd0e7a8edf8315b16ec50449dd6b721f
Komilxon <komilkhon.asamov@hotmail.com>
2016-05-21 13:28:25 +0500

    Render development stages from db

--- face/views.py
+++ face/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Project, TeamMember, CustomerMessage
+from .models import Project, TeamMember, CustomerMessage, DevelopmentStage
 
 
 def get_index_page(request):
@@ -16,4 +16,5 @@ def get_index_page(request):
         return render(request, 'face/index.html', {
             'projects': Project.objects.all(),
             'team_members': TeamMember.objects.all(),
+            'development_stages': DevelopmentStage.objects.all(),
         })
