126d9fe21784e1c823a4c9f726c861af86e290e7
Komilxon <komilkhon.asamov@hotmail.com>
2016-07-30 13:54:11 +0500

    Add sending email settings
    Email user and pass meant to be imported from deploy settings.

--- webstudio/settings.py
+++ webstudio/settings.py
@@ -142,6 +142,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 THUMBNAIL_BASEDIR = 'thumbnails'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'name@email.com'
+EMAIL_HOST_PASSWORD = 'password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
 try:
 	from .deploy_settings import *
 except ImportError:
