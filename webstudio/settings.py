5ddd7464c8aaf41f9ab4815aca43ec3d20320bb7
Komilxon <komilkhon.asamov@hotmail.com>
2016-07-31 15:41:54 +0500

    Change email provider from gmail to yandex

--- webstudio/settings.py
+++ webstudio/settings.py
@@ -143,11 +143,12 @@ THUMBNAIL_BASEDIR = 'thumbnails'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_HOST_USER = 'name@email.com'
 EMAIL_HOST_PASSWORD = 'password'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_SEND_TO = ['name2@email.com']
 
 
