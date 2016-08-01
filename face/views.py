412c60f44babcdd6c4a6070958dd072beeec2526
Komilxon <komilkhon.asamov@hotmail.com>
2016-08-01 16:53:58 +0500

    Change tabs to spaces in python files

--- face/views.py
+++ face/views.py
@@ -7,35 +7,35 @@ from .models import Project, TeamMember, CustomerMessage, DevelopmentStage, Serv
 
 
 def get_index_page(request):
-	if request.method == 'POST':
-		message = CustomerMessage.objects.create(
-			name=request.POST['name'],
-			email=request.POST['email'],
-			phone=request.POST['phone'],
-			message=request.POST['message'],
-		)
-		notificate_team_about_new_message(message)
-		return HttpResponse('ok')
-	else:
-		return render(request, 'face/index.html', {
-			'projects': Project.objects.all(),
-			'team_members': TeamMember.objects.all(),
-			'development_stages': DevelopmentStage.objects.all(),
-			'services': Service.objects.all(),
-			'year': datetime.datetime.now().year
-		})
+    if request.method == 'POST':
+        message = CustomerMessage.objects.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            phone=request.POST['phone'],
+            message=request.POST['message'],
+        )
+        notificate_team_about_new_message(message)
+        return HttpResponse('ok')
+    else:
+        return render(request, 'face/index.html', {
+            'projects': Project.objects.all(),
+            'team_members': TeamMember.objects.all(),
+            'development_stages': DevelopmentStage.objects.all(),
+            'services': Service.objects.all(),
+            'year': datetime.datetime.now().year
+        })
 
 def notificate_team_about_new_message(message):
-	subject = 'Kirapps - новое сообщение'
-	message_template = 'Имя: {name}\nEmail: {email}\nPhone: {phone}\n' + \
-		'Message: {message}\nTime sent: {time_sent}'
-	message = message_template.format(
-		name=message.name,
-		email=message.email,
-		phone=message.phone,
-		message=message.message,
-		time_sent=message.time_sent
-	)
+    subject = 'Kirapps - новое сообщение'
+    message_template = 'Имя: {name}\nEmail: {email}\nPhone: {phone}\n' + \
+        'Message: {message}\nTime sent: {time_sent}'
+    message = message_template.format(
+        name=message.name,
+        email=message.email,
+        phone=message.phone,
+        message=message.message,
+        time_sent=message.time_sent
+    )
 
-	send_mail(subject, message, settings.EMAIL_HOST_USER,
-		settings.EMAIL_SEND_TO, fail_silently=False)
+    send_mail(subject, message, settings.EMAIL_HOST_USER,
+        settings.EMAIL_SEND_TO, fail_silently=False)
