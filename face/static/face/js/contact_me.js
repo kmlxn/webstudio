521fe7a9db04ed99a2ce4a8e609217f9b387531f
Komilxon <komilkhon.asamov@hotmail.com>
2016-08-01 16:01:56 +0500

    Add translation to feedback delivery statuses
    Also "sending" spinner added

--- face/static/face/js/contact_me.js
+++ face/static/face/js/contact_me.js
@@ -7,6 +7,9 @@ $(function() {
         },
         submitSuccess: function($form, event) {
             event.preventDefault(); // prevent default submit behaviour
+            $('#message_send_success').hide();
+            $('#message_send_error').hide();
+            $('#message_sending').show();
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -31,26 +34,15 @@ $(function() {
                 },
                 cache: false,
                 success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                    $('#message_sending').hide();
+                    $('#message_send_success').show();
 
-                    //clear all fields
                     $('#contactForm').trigger("reset");
                 },
                 error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
+                    $('#message_sending').hide();
+                    $('#message_send_error').show();
+
                     $('#contactForm').trigger("reset");
                 },
             })
